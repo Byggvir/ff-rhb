3c096114d4174a0b699aec927575093d99dc7317
Byggvir <thomas@arend-rhb.de>
2015-08-16 20:34:45 +0200

    Jetzt sollte es mit dem commit klappen.

---
+++ site/site.mk
@@ -0,0 +1,36 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-14 \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-legacy \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged
+
+
+DEFAULT_GLUON_RELEASE := 0.8~exp$(shell date '+%Y%m%d')
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+
+GLUON_LANGS ?= en de
