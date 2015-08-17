567560c93519dbe6d976470863cc4350f3c8b9f7
Byggvir <thomas@arend-rhb.de>
2015-08-17 23:15:40 +0200

    Geänderte Konfiguration nachdem Firmware kein WIFI unterstützte.

--- site/site.mk
+++ site/site.mk
@@ -1,36 +1,36 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
-	gluon-alfred \
-	gluon-announced \
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-wifi-config \
-	gluon-next-node \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	iwinfo \
-	iptables \
-	haveged
-
+    gluon-mesh-batman-adv-15 \
+    gluon-alfred \
+    gluon-announced \
+    gluon-neighbour-info \
+    gluon-autoupdater \
+    gluon-setup-mode \
+    gluon-config-mode-core \
+    gluon-config-mode-autoupdater \
+    gluon-config-mode-hostname \
+    gluon-config-mode-mesh-vpn \
+    gluon-config-mode-geo-location \
+    gluon-config-mode-contact-info \
+    gluon-ebtables-filter-multicast \
+    gluon-ebtables-filter-ra-dhcp \
+    gluon-legacy \
+    gluon-luci-admin \
+    gluon-luci-autoupdater \
+    gluon-luci-portconfig \
+    gluon-luci-private-wifi \
+    gluon-luci-node-role \
+    gluon-next-node \
+    gluon-mesh-vpn-fastd \
+    gluon-radvd \
+    gluon-status-page \
+    iwinfo \
+    iptables \
+    haveged 
 
 DEFAULT_GLUON_RELEASE := 0.8~exp$(shell date '+%Y%m%d')
-
 # Allow overriding the release number from the command line
+
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
-
+# GLUON_SITEDIR=site-siegburg/
 GLUON_PRIORITY ?= 0
-
 GLUON_LANGS ?= en de
