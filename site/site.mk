a7128ceaf059c619ec8dde126383da2122fc436e
Byggvir <thomas@arend-rhb.de>
2015-08-18 21:05:32 +0200

    Anpassung wifi24 und wifi25, da wifi nicht richtig gestarte wird. Zusätzlich das Packet gluon-authorized-keys damit der ssh-key übrenommen wird. Bisherige Firmware übernahm weder wifi nocht ssh-keys.

--- site/site.mk
+++ site/site.mk
@@ -1,31 +1,33 @@
 GLUON_SITE_PACKAGES := \
-    gluon-mesh-batman-adv-15 \
-    gluon-alfred \
-    gluon-announced \
-    gluon-neighbour-info \
-    gluon-autoupdater \
-    gluon-setup-mode \
-    gluon-config-mode-core \
-    gluon-config-mode-autoupdater \
-    gluon-config-mode-hostname \
-    gluon-config-mode-mesh-vpn \
-    gluon-config-mode-geo-location \
-    gluon-config-mode-contact-info \
-    gluon-ebtables-filter-multicast \
-    gluon-ebtables-filter-ra-dhcp \
-    gluon-legacy \
-    gluon-luci-admin \
-    gluon-luci-autoupdater \
-    gluon-luci-portconfig \
-    gluon-luci-private-wifi \
-    gluon-luci-node-role \
-    gluon-next-node \
-    gluon-mesh-vpn-fastd \
-    gluon-radvd \
-    gluon-status-page \
-    iwinfo \
-    iptables \
-    haveged 
+gluon-alfred \
+gluon-announced \
+gluon-authorized-keys \
+gluon-autoupdater \
+gluon-config-mode-autoupdater \
+gluon-config-mode-contact-info \
+gluon-config-mode-core \
+gluon-config-mode-geo-location \
+gluon-config-mode-hostname \
+gluon-config-mode-mesh-vpn \
+gluon-ebtables-filter-multicast \
+gluon-ebtables-filter-ra-dhcp \
+gluon-legacy \
+gluon-luci-admin \
+gluon-luci-autoupdater \
+gluon-luci-node-role \
+gluon-luci-portconfig \
+gluon-luci-private-wifi \
+gluon-luci-wifi-config \
+gluon-mesh-batman-adv-15 \
+gluon-mesh-vpn-fastd \
+gluon-neighbour-info \
+gluon-next-node \
+gluon-radvd \
+gluon-setup-mode \
+gluon-status-page \
+haveged \
+iptables \
+iwinfo
 
 DEFAULT_GLUON_RELEASE := 0.8~exp$(shell date '+%Y%m%d')
 # Allow overriding the release number from the command line
